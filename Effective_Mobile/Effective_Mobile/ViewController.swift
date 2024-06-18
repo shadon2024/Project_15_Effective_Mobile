ec5429933d8ae13ca20958df9bc458f8ac3e12a1
shadon2024 <in-con23@yandex.ru>
2024-06-18 18:56:01 +0500

    creat project

---
+++ Effective_Mobile/Effective_Mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Effective_Mobile
+//
+//  Created by Admin on 18/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
